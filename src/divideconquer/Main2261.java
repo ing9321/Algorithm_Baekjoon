997c26306c2a3e54f92deff1c566dde0da4d2e81
EunG <ing9321@gmail.com>
2019-02-19 18:42:08 +0900

    19/02/19 용돈관리 해결, 두 점 사이 거리 시작

---
+++ src/divideconquer/Main2261.java
@@ -0,0 +1,67 @@
+package divideconquer;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main2261 {
+	static TreeSet<Point> points;
+	static int MIN;
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("res/input2261.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine().trim());
+		points = new TreeSet<Point>();
+		MIN = Integer.MAX_VALUE;
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			points.add(new Point(x,y));
+		}
+
+		System.out.println(points);
+	}
+
+	static class Point implements Comparable<Point> {
+		int x;
+		int y;
+//		Point minP;
+//		int min = Integer.MAX_VALUE;
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			builder.append("Point [x=");
+			builder.append(x);
+			builder.append(", y=");
+			builder.append(y);
+//			builder.append(", min=");
+//			builder.append(min);
+			builder.append("]");
+			return builder.toString();
+		}
+
+		@Override
+		public int compareTo(Point o) {
+//			System.out.println(this);
+//			System.out.println(o);
+//			System.out.println("=============");
+//			int xx = (o.x - this.x);
+//			int yy = (o.y - this.y);
+//			int len = (xx * xx) + (yy * yy);
+//			if(len < this.min) {
+//				this.minP = o;
+//				this.min = len;
+//			}
+
+			return this.x - o.x > 0 ? 1 : -1;
+		}
+	}
+}
--- src/divideconquer/Main6236.java
+++ src/divideconquer/Main6236.java
@@ -4,11 +4,50 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 
+// 용돈관리
 public class Main6236 {
-
+	static int N;
+	static int[] days;
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("res/input6236.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		String[] strarr = br.readLine().split(" ");
+		N = Integer.parseInt(strarr[0]);
+		int M = Integer.parseInt(strarr[1]);
+		days = new int[N];
+		int MIN = 0;
+		int MAX = 0;
+		for(int i=0; i<N; i++) {
+			days[i] = Integer.parseInt(br.readLine().trim());
+			MIN = Math.max(MIN, days[i]);
+			MAX += days[i];
+		}
+
+		while(MIN < MAX) {
+			System.out.println(MIN + " " + MAX);
+			int mid = (MIN + MAX) / 2;
+			if(withraw(mid) > M) {
+				// 중간값 넣고 돌렸을 때 인출 횟수가 많으면 중간값을 증가시켜서 분할
+				MIN = mid+1;
+			} else {
+				// 중간값만큼 인출했을 때 횟수가 모자라므로 중간값 밑으로 분할
+				MAX = mid;
+			}
+		}
+		System.out.println(MIN);
+	}
+	private static int withraw(int money) {
+		int m = 1;
+		int cash = money;
+		for(int i=0; i<N; i++) {
+			if(days[i] > cash) {
+				m++;
+				cash = money;
+			}
+			cash -= days[i];
+			System.out.println("current : " + cash);
+		}
+		System.out.println("withraw : " + m);
+		return m;
 	}
 }
