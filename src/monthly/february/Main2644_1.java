2bc7ff4df08c3487479a5d9e3f37ac3a2fa828a4
EunG <ing9321@gmail.com>
2019-02-25 18:17:44 +0900

    19/02/25 촌수계산 2차원 배열로 성공

--- src/monthly/february/Main2644_1.java
+++ src/monthly/february/Main2644_1.java
@@ -8,62 +8,43 @@ import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 // 촌수계산
-public class Main2645_1 {
-	static int N, M, p1, p2, cnt;
+public class Main2644_1 {
+	static int N, M, p1, p2;
 	static int[][] matrix;
-	static boolean[] visit;
+	static int[] depth;
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("res/input2644.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine().trim()); // 전체 사람 수
 		matrix = new int[N+1][N+1];
-		visit = new boolean[N+1];
+		depth = new int[N+1];
 		String[] strarr = br.readLine().split(" ");
 		p1 = Integer.parseInt(strarr[0]); // 시작점
 		p2 = Integer.parseInt(strarr[1]); // 도착점
 		M = Integer.parseInt(br.readLine().trim());		// 관계의 개수
-		cnt = 0;
 		for(int i=0; i<M; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int p = Integer.parseInt(st.nextToken().trim());
 			int c = Integer.parseInt(st.nextToken().trim());
 			matrix[p][c] = 1;
-			matrix[c][p] = 1;			
+			matrix[c][p] = 1;
 		}
 		
-		for (int i = 1; i <= N; i++) {
-			System.out.println(i + " : " + matrix[i]);
-		}
-
-		bfs();
-		System.out.println(cnt);
-	}
-	private static void bfs() {
 		LinkedList<Integer> queue = new LinkedList<Integer>();
 		queue.offer(p1);
-		int sub = 0;
 		while(!queue.isEmpty()) {
-			System.out.println(queue);
 			int current = queue.poll();
-			if(current == p2) {
-				break;
-			}
-			if(!visit[current]) {
-				System.out.println("visit : " + current);
-				visit[current] = true; // 방문처리
-//				Node temp = matrix[current];
-//				cnt++;
-//				while(temp != null) {
-//					sub++;
-//					if(!visit[temp.vertex]) {
-//						queue.offer(temp.vertex);
-//					}
-//					temp = temp.link;
-//				}
+			for(int i=1; i<=N; i++) {
+				if(matrix[current][i] == 1 && depth[i] == 0) {
+					depth[i] = depth[current] + 1;
+					queue.offer(i);
+				}
 			}
 		}
-		System.out.println(sub);
-		System.out.println(Arrays.toString(visit));
+		if(depth[p2] != 0) {
+			System.out.println(depth[p2]);
+		} else {
+			System.out.println(-1);
+		}
 	}
-
 }
