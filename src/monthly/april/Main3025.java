b00e6edd40e8352b16f2f9786a77d4d1522a7555
EunG <ing9321@gmail.com>
2019-03-14 18:57:51 +0900

    19/03/14 돌던지기 시간초과..

--- src/monthly/april/Main3025.java
+++ src/monthly/april/Main3025.java
@@ -1,55 +1,55 @@
 package monthly.april;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.OutputStreamWriter;
 
 // 돌 던지기
 public class Main3025 {
 	static int R, C;
-	static int[][] map;
+	static char[][] map;
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("res/input3025.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		String[] rc = br.readLine().split(" ");
 		R = Integer.parseInt(rc[0]);
 		C = Integer.parseInt(rc[1]);
-		map = new int[R][C];
+		map = new char[R][C];
 		for (int i = 0; i < R; i++) {
-			String line = br.readLine();
-			for (int j = 0; j < C; j++) {
-				if(line.charAt(j) == 'X') map[i][j] = 2;
-			}
+			map[i] = br.readLine().toCharArray();
 		}
 		
 		int N = Integer.parseInt(br.readLine().trim());
 		for (int i = 0; i < N; i++) {
-			int c = Integer.parseInt(br.readLine().trim()) - 1;
-			go(0, c);
+			go(0, Integer.parseInt(br.readLine().trim()) - 1);
 		}
 		
 		for (int i = 0; i < R; i++) {
-			System.out.println(Arrays.toString(map[i]));
+			bw.write(map[i]);
+			bw.write("\n");
 		}
+		bw.flush();
 	}
 	private static void go(int r, int c) {
 		if(r+1 < R) {
-			if(map[r+1][c] == 0) {
+			if(map[r+1][c] == '.') {
 				go(r+1, c);
-			} else if(map[r+1][c] == 2) {
-				map[r][c] = 1;
-			} else if(map[r+1][c] == 1) {
-				if(c > 0 && map[r+1][c-1] == 0) {
+			} else if(map[r+1][c] == 'X') {
+				map[r][c] = 'O';
+			} else if(map[r+1][c] == 'O') {
+				if(c > 0 && map[r+1][c-1] == '.') {
 					go(r+1, c-1);
-				} else if(c-1 < C && map[r+1][c+1] == 0) {
+				} else if(c+1 < C && map[r+1][c+1] == '.') {
 					go(r+1, c+1);
 				} else {
-					map[r][c] = 1;
+					map[r][c] = 'O';
 				}
 			}
 		} else {
-			map[r][c] = 1;
+			map[r][c] = 'O';
 		}
 	}
 }
