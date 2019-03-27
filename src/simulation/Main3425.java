979c47e5a749543ac968641a680296a3c033f266
EunG <ing9321@gmail.com>
2019-03-27 18:27:58 +0900

    19/03/27 고스택 재시도 중..

--- src/simulation/Main3425.java
+++ src/simulation/Main3425.java
@@ -13,155 +13,137 @@ import java.util.Stack;
  * Simulation
  */
 public class Main3425 {
+	private static class Gostack {
+		int INF = 1000000000;
+		Stack<Integer> stack;
+		public Gostack() {
+			this.stack = new Stack<Integer>();
+		}
+		private boolean num(int x) { // x를 스택의 가장 위에 저장한다. (0 <= x <= 109)
+			stack.push(x);
+			return true;
+		}
+		private boolean pop() { // 스택 가장 위의 숫자를 제거한다.
+			if(!stack.isEmpty()) {
+				stack.pop();
+				return true;
+			}
+			return false;
+		}
+		private boolean inv() { // 첫번째 수의 부호를 바꾼다.
+			if(!stack.isEmpty()) {
+				int n = stack.pop();
+				n = n * (-1);
+				if(n > INF) return false;
+				stack.push(n);
+				return true;
+			}
+			return false;
+		}
+		private boolean dup() { // 첫번째 숫자를 하나 더 스택의 가장 위에 저장한다.
+			if(!stack.isEmpty()) {
+				int n = stack.peek();
+				stack.push(n);
+				return true;
+			}
+			return false;
+		}
+		private boolean swp() { // 첫 번째 숫자와 두 번째 숫자의 위치를 서로 바꾼다.
+			if(stack.size() > 1) {
+				int n1 = stack.pop();
+				int n2 = stack.pop();
+				stack.push(n1);
+				stack.push(n2);
+				return true;
+			}
+			return false;
+		}
+		private boolean add() { // 첫 번째 숫자와 두 번째 숫자를 더한다.
+			if(stack.size() > 1) {
+				int n1 = stack.pop();
+				int n2 = stack.pop();
+				int n = n1 + n2;
+				if(n > INF) return false;
+				stack.push(n);
+				return true;
+			}
+			return false;
+		}
+		private boolean sub() { // 첫 번째 숫자와 두 번째 숫자를 뺀다. (두 번째 - 첫 번쨰)
+			if(stack.size() > 1) {
+				int n1 = stack.pop();
+				int n2 = stack.pop();
+				int n = n2 - n1;
+				stack.push(n);
+				return true;
+			}
+			return false;
+		}
+		private boolean mul() { // 첫번째 숫자와 두 번째 숫자를 곱한다.
+			if(stack.size() > 1) {
+				int n1 = stack.pop();
+				int n2 = stack.pop();
+				int n = n1 * n2;
+				if(n > INF) return false;
+				stack.push(n);
+				return true;
+			}
+			return false;
+		}
+		private boolean div() { // 첫 번째 숫자(제수)로 두 번째 숫자(피제수)를 나눈 몫을 저장한다.
+			if(stack.size() > 1) {
+				int n1 = stack.pop();
+				int n2 = stack.pop();
+				if(n1 == 0) return false;
+				int n11 = Math.abs(n1);
+				int n22 = Math.abs(n2);
+				int n = n22 / n11;
+				if(n1 * n2 < 0) { // 곱해서 음수면 둘 중 하나만 음수
+					n = n * (-1);
+				}
+				stack.push(n);
+				return true;
+			}
+			return false;
+		}
+		private boolean mod() { // 첫 번째 숫자(제수)로 두 번째 숫자(피제수)를 나눈 나머지를 저장한다.
+			if(stack.size() > 1) {
+				int n1 = stack.pop();
+				int n2 = stack.pop();
+				if(n1 == 0) return false;
+				int n11 = Math.abs(n1);
+				int n22 = Math.abs(n2);
+				int n = n22 % n11;
+				if(n1 < 0) { // 피제수가 음수면 나머지 부호도 음수
+					n = n * (-1);
+				}
+				stack.push(n);
+				return true;
+			}
+			return false;
+		}
+		private String get() {
+			if(stack.size() == 1) {
+				int n = stack.pop();
+				return n+"";
+			}
+			return "ERROR";
+		}
+	}
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("res/input3425.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine().trim();
 		StringBuilder sb = new StringBuilder();
-		while(!str.equals("QUIT")) {
-			ArrayList<String> list = new ArrayList<String>();
-			while(!str.equals("END")) {
-				list.add(str);
-				str = br.readLine().trim();
-			}
-			list.add("END");
-//			System.out.println(list);
-			
-			int N = Integer.parseInt(br.readLine().trim());
-			for (int i = 0; i < N; i++) {
-				Stack<Integer> stack = new Stack<Integer>();
-				stack.push(Integer.parseInt(br.readLine().trim()));
-//				System.out.println(stack.peek());
-				calc:
-				for (int j = 0; j < list.size(); j++) {
-					String order = list.get(j);
-					if(order.contains("NUM")) {
-						int n1 = Integer.parseInt(order.split(" ")[1]);
-						stack.push(n1);
-					} else if(order.contains("POP")) {
-						if(stack.isEmpty()) {
-							sb.append("ERROR\n");
-							break calc;
-						}
-						stack.pop();
-					} else if(order.contains("INV")) {
-						if(stack.isEmpty()) {
-							sb.append("ERROR\n");
-							break calc;
-						}
-						int n1 = stack.pop();
-						n1 *= -1;
-						stack.push(n1);
-					} else if(order.contains("DUP")) {
-						if(stack.isEmpty()) {
-							sb.append("ERROR\n");
-							break calc;
-						}
-						int n1 = stack.peek();
-						stack.push(n1);
-					} else if(order.contains("SWP")) {
-						if(stack.size() < 2) {
-							sb.append("ERROR\n");
-							break calc;
-						}
-						int n1 = stack.pop();
-						int n2 = stack.pop();
-						stack.push(n1);
-						stack.push(n2);
-					} else if(order.contains("ADD")) {
-						if(stack.size() < 2) {
-							sb.append("ERROR\n");
-							break calc;
-						}
-						int n1 = stack.pop();
-						int n2 = stack.pop();
-						int temp = n1+n2;
-						stack.push(temp);
-					} else if(order.contains("SUB")) {
-						if(stack.size() < 2) {
-							sb.append("ERROR\n");
-							break calc;
-						}
-						int n1 = stack.pop();
-						int n2 = stack.pop();
-						int temp = n2-n1;
-						stack.push(temp);
-					} else if(order.contains("MUL")) {
-						if(stack.size() < 2) {
-							sb.append("ERROR\n");
-							break calc;
-						}
-						int n1 = stack.pop();
-						int n2 = stack.pop();
-						int temp = n2 * n1;
-						stack.push(temp);
-					} else if(order.contains("DIV")) {
-						if(stack.size() < 2) {
-							sb.append("ERROR\n");
-							break calc;
-						}
-						int n1 = stack.pop();
-						if(n1 == 0) {
-							sb.append("ERROR\n");
-							break calc;
-						}
-						int n2 = stack.pop();
-						int cnt = 0;
-						if(n1 < 0) {
-							n1 *= -1;
-							cnt++;
-						}
-						if(n2 < 0) {
-							n2 *= -1;
-							cnt++;
-						}
-						int temp = n2 / n1;
-						if(cnt == 1) {
-							temp *= -1;
-						}
-						stack.push(temp);
-					} else if(order.contains("MOD")) {
-						if(stack.size() < 2) {
-							sb.append("ERROR\n");
-							break calc;
-						}
-						int n1 = stack.pop();
-						if(n1 == 0) {
-							sb.append("ERROR\n");
-							break calc;
-						}
-						int n2 = stack.pop();
-						int n11 = n1;
-						if(n1 < 0) {
-							n1 *= -1;
-						}
-						if(n2 < 0) {
-							n2 *= -1;
-						}
-						int temp = n2 % n1;
-						if(n11 < 0) {
-							temp *= -1;
-						}
-						stack.push(temp);
-					} else if(order.contains("END")) {
-						if(stack.size() == 1) {
-							sb.append(stack.peek()).append("\n");
-							break calc;
-						} else {
-							sb.append("ERROR\n");
-							break calc;
-						}
-					}
-					if(stack.peek() > 1000000000) {
-						sb.append("ERROR\n");
-						break calc;
-					}
-				}
-			}
-			sb.append("\n");
-			br.readLine();
-			str = br.readLine().trim();
+		Gostack gostack = new Gostack();
+		ArrayList<String> list = new ArrayList<String>();
+		start:
+		while(true) {
+			String str = br.readLine().trim();
+			if(str.equals("QUIT")) break;
+			System.out.println(str);
 		}
+		
+		
 		System.out.print(sb);
 	}
 }
---
+++ src/simulation/Main5373.java
@@ -0,0 +1,20 @@
+package simulation;
+
+/*
+ * 미해결
+ * 5373 큐빙
+ * https://www.acmicpc.net/problem/5373
+ * Simulation, 루믹스 큐브
+ */
+public class Main5373 {
+	static char[][][] cube = {
+			{{'w','w','w'}, {'w','w','w'},{'w','w','w'}},
+			{ },
+			{ },
+	};
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
