19cd09f2ee5d4b863c6b2d83afade29ff79b19a4
SOPHIE <ing9321@gmail.com>
2019-09-10 03:06:53 +0900

    190909 코딩테스트 준비

--- src/arithmetic/Main1546.java
+++ src/arithmetic/Main1546.java
@@ -3,39 +3,28 @@ package arithmetic;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /*
  * 1546 평균
  * https://www.acmicpc.net/problem/1546
- * 점수/M*100
- * 새로운 평균을 구하는 프로그램
- * 첫째 줄에 시험 본 과목의 개수 N이 주어진다. 이 값은 1000보다 작거나 같다. 
- * 둘째 줄에 세준이의 현재 성적이 주어진다. 이 값은 100보다 작거나 같은 음이 아닌 정수이고, 적어도 하나의 값은 0보다 크다.
- * .00까지
  */
 public class Main1546 {
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("res/input1546.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine().trim());
-		double[] subjects = new double[N];
-		double M = 0;
+		int M = 0;
+		int sum = 0;
 		StringTokenizer st = new StringTokenizer(br.readLine().trim());
 		for (int i = 0; i < N; i++) {
-			subjects[i] = Double.parseDouble(st.nextToken().trim());
-			if(M < subjects[i]) M = subjects[i];
+			int tmp = Integer.parseInt(st.nextToken().trim());
+			sum += tmp;
+			if(M < tmp) M = tmp;
 		}
-		
-		int avg = 0;
-		for (int i = 0; i < N; i++) {
-			subjects[i] = subjects[i] / M * 100;
-			avg += subjects[i];
-		}
-		
-		System.out.println(Arrays.toString(subjects));
-		System.out.printf("%.2f", avg / (double)N);
+
+		double avg = 100.0 * sum / M / N;
+		System.out.printf("%.2f", avg);
 	}
 
 }
--- src/skcc_study/Main7562.java
+++ src/skcc_study/Main7562.java
@@ -3,16 +3,16 @@ package skcc_study;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 
 /*
  * 7562 나이트의 이동
  * https://www.acmicpc.net/problem/7562
- * 
+ * bfs
  */
 public class Main7562 {
 	public static int I;
+	public static int[][] visit;
 	public static int[][] dir = {{2,1}, {1,2}, {-1,2}, {-2,1}, {-2,-1}, {-1,-2}, {1,-2}, {2,-1}};
 	public static LinkedList<int[]> queue;
 	public static int[] dest;
@@ -23,39 +23,33 @@ public class Main7562 {
 		int T = Integer.parseInt(br.readLine().trim());
 		for (int tc = 0; tc < T; tc++) {
 			I = Integer.parseInt(br.readLine().trim());
+			visit = new int[I][I];
 			queue = new LinkedList<int[]>();
 			String[] str = br.readLine().trim().split(" ");
 			queue.add(new int[] {Integer.parseInt(str[0]), Integer.parseInt(str[1])});
 			str = br.readLine().trim().split(" ");
 			dest = new int[] {Integer.parseInt(str[0]), Integer.parseInt(str[1])};
+			visit[queue.peek()[0]][queue.peek()[1]] = 1;
 			
 			int cnt = 0;
-			if(queue.peek()[0] == dest[0] && queue.peek()[1] == dest[1]) {
-				
-			} else {
-				start:while(!queue.isEmpty()) {
-					int size = queue.size();
-					cnt++;
-					for (int i = 0; i < size; i++) {
-						int[] cur = queue.poll();
-						System.out.println(cur[0] + " " + cur[1]);
-						for (int j = 0; j < 8; j++) {
-							int nr = cur[0] + dir[j][0];
-							int nc = cur[1] + dir[j][1];
-							if(nr < 0 || nr >= I || nc < 0 || nc >= I) continue;
-							if(nr == dest[0] && nc == dest[1]) {
-								break start;
-							} else {
-								queue.add(new int[] {nr, nc});
-							}
-						}
+			start:while(!queue.isEmpty()) {
+				int size = queue.size();
+				for (int i = 0; i < size; i++) {
+					int[] cur = queue.poll();
+					if(cur[0] == dest[0] && cur[1] == dest[1]) {
+						break start;
+					}
+					for (int j = 0; j < 8; j++) {
+						int nr = cur[0] + dir[j][0];
+						int nc = cur[1] + dir[j][1];
+						if(nr < 0 || nr >= I || nc < 0 || nc >= I || visit[nr][nc] == 1) continue;
+						visit[nr][nc] = 1;
+						queue.add(new int[] {nr, nc});
 					}
 				}
+				cnt++;
 			}
-			
 			System.out.println(cnt);
-			
 		}
 	}
-	
 }
